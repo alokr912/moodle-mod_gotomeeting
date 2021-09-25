eb250a69b9539b3bd1314e7a9a30bb4211e3c825
Alok Kumar <alokr.mail@gmail.com>
2021-09-25 17:31:58 +0530

    Commit after analytics added

--- db/install.php
+++ db/install.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of the GoToMeeting plugin for Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -24,5 +25,36 @@
 defined('MOODLE_INTERNAL') || die();
 
 function xmldb_gotomeeting_install() {
+    global $DB, $CFG;
+    $ch = curl_init('https://api.mdlintegration.com/v1/public/gotoinstance');
+    curl_setopt($ch, CURLOPT_POST, true);
+    $headers = [
+        'Content-Type: application/json'
+    ];
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $data = array();
+    $data['wwwroot'] = $CFG->wwwroot;
+    $data['siteidentifier'] = $CFG->siteidentifier;
+    $data['country'] = $CFG->country;
+
+    $data['autolang'] = $CFG->autolang;
+    $data['lang'] = $CFG->lang;
+    $data['supportname'] = $CFG->supportname;
+
+    $data['supportemail'] = $CFG->supportemail;
+    $data['release'] = $CFG->release;
+    $data['branch'] = $CFG->branch;
+
+    $data['os'] = $CFG->os;
+    $data['timezone'] = $CFG->timezone;
+    $data['ostype'] = $CFG->ostype;
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_exec($ch);
+
+    curl_close($ch);
     return true;
 }
